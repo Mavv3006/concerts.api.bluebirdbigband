dd87b40adc1d03fc060a57cee9fb21c9721bd41f
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2021-08-21 17:48:59 +0200

    added old api

--- app/Http/Controllers/ConcertsController.php
+++ app/Http/Controllers/ConcertsController.php
@@ -5,32 +5,42 @@ namespace App\Http\Controllers;
 
 
 use App\Http\Resources\ConcertResource;
+use App\Http\Resources\OldConcertResource;
 use App\Models\Concert;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Carbon;
 
 class ConcertsController extends Controller
 {
-    public function all()
+    public function all(): JsonResponse
     {
-        return ConcertResource::collection(Concert::with('band', 'place')->get());
+        return (ConcertResource::collection(Concert::with('band', 'place')->get()))
+            ->response()
+            ->header('Access-Control-Allow-Origin', '*');
     }
 
-    public function upcoming()
+    public function upcoming(): JsonResponse
     {
-        return ConcertResource::collection($this->upcoming_concerts());
+        return (ConcertResource::collection($this->upcoming_concerts()))
+            ->response()
+            ->header('Access-Control-Allow-Origin', '*');
     }
 
-    public function past()
+    public function past(): JsonResponse
     {
         $concerts = Concert::with('band', 'place')
             ->whereDate('date', '<', Carbon::today()->toDateString())
             ->get();
-        return ConcertResource::collection($concerts);
+        return (ConcertResource::collection($concerts))
+            ->response()
+            ->header('Access-Control-Allow-Origin', '*');
     }
 
-    public function old_upcoming()
+    public function old_upcoming(): JsonResponse
     {
-
+        return (OldConcertResource::collection($this->upcoming_concerts()))
+            ->response()
+            ->header('Access-Control-Allow-Origin', '*');
     }
 
     private function upcoming_concerts()
