55d625b6c5934931aca5e9ead7888142c00b0c82
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2022-04-04 19:58:41 +0200

    added more tests in ConcertsControllerTest.php

--- tests/Http/Controllers/ConcertsControllerTest.php
+++ tests/Http/Controllers/ConcertsControllerTest.php
@@ -12,11 +12,61 @@ class ConcertsControllerTest extends TestCase
 {
     use DatabaseMigrations;
 
-    /*
-     * TODO: add test to verify response structure
-     * TODO: add test for past concerts when there are concerts for in the future in the DB
-     * TODO: add test to verify that /concerts/all returns all concerts available in the DB
-     * */
+    public function test_past_concerts_with_concerts_for_in_the_future()
+    {
+        $this->generate_future_concert();
+        $this->get('concerts/past')->seeJsonEquals([]);
+    }
+
+    public function test_concerts_all_returns_all_concerts()
+    {
+        $this->generate_future_concert();
+        $this->generate_past_concert();
+
+        $this->assertCount(2, Concert::all());
+        $response_count = $this
+            ->get('concerts/all')
+            ->response
+            ->content();
+
+        $this->assertCount(2, json_decode($response_count, true));
+        $this->assertCount(2, json_decode($response_count, false));
+    }
+
+    public function test_response_structure()
+    {
+        $json_structure = [
+            '*' => [
+                'date',
+                'start_time',
+                'end_time',
+                'band_name',
+                'location' => [
+                    'street',
+                    'number',
+                    'plz',
+                    'name'
+                ]
+            ]
+        ];
+
+        Concert::factory()->create();
+
+        $this
+            ->get('concerts/all')
+            ->seeStatusCode(200)
+            ->seeJsonStructure($json_structure);
+
+        $this
+            ->get('concerts/upcoming')
+            ->seeStatusCode(200)
+            ->seeJsonStructure($json_structure);
+
+        $this
+            ->get('concerts/past')
+            ->seeStatusCode(200)
+            ->seeJsonStructure($json_structure);
+    }
 
     public function test_upcoming_including_today()
     {
@@ -27,19 +77,12 @@ class ConcertsControllerTest extends TestCase
         $concert = Concert::first();
 
         $this->json('GET', 'concerts/upcoming')
-            ->seeJsonContains([
-                'date' => $concert->date(),
-            ]);
+            ->seeJsonContains(['date' => $concert->date()]);
     }
 
     public function test_upcoming_past_start_time()
     {
-        $time = new DateTime();
-        $time->modify('-2 hours');
-        Concert::factory()->create([
-            'date' => Carbon::today()->toDateString(),
-            'start_time' => $time->format('H:i:s'),
-        ]);
+        $this->generate_past_concert();
 
         $count = $this
             ->get('concerts/upcoming')
@@ -67,5 +110,31 @@ class ConcertsControllerTest extends TestCase
         $this
             ->get('concerts/past')
             ->seeJsonStructure(['*' => []]);
+    }
+
+    /**
+     * @return void
+     */
+    private function generate_future_concert(): void
+    {
+        $time = new DateTime();
+        $time->modify('+2 hours');
+        Concert::factory()->create([
+            'date' => Carbon::today()->toDateString(),
+            'start_time' => $time->format('H:i:s'),
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    private function generate_past_concert(): void
+    {
+        $time = new DateTime();
+        $time->modify('-2 hours');
+        Concert::factory()->create([
+            'date' => Carbon::yesterday()->toDateString(),
+            'start_time' => $time->format('H:i:s'),
+        ]);
     }
 }
