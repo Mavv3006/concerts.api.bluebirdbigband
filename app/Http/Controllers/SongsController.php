cfeac1da154d5bd642dab6b53dd3cc72bfbf14c0
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2022-04-07 20:46:42 +0200

    finished refactoring SongsController.php

--- app/Http/Controllers/SongsController.php
+++ app/Http/Controllers/SongsController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class SongsController extends Controller
@@ -37,47 +36,24 @@ class SongsController extends Controller
 
         if ($validator->fails()) {
             Log::warning("[SongController] 'file_name' query parameter is not set.");
-            //  https://code-boxx.com/convert-array-string-php/
-            $array = $validator->errors()->messages();
-            var_dump(implode(" ", $array));
+            $mapping_fnc = fn($elem) => $elem[0];
+            $message = implode(' ', array_map($mapping_fnc, $validator->errors()->messages()));
             return response()->json([
                 'error' => "Required 'file_name' query parameter is not set",
-                'message' => $validator->errors()->messages()
+                'message' => $message
             ], 400);
         }
-        try {
 
-            $credentials = $this->validate($request, [
-                'file_name' => [
-                    'required',
-                    'filled',
-                    function ($attribute, $value, $fail) use ($request) {
-                        $is_query_param = $request->query('file_name') == $value;
-                        if (!$is_query_param) {
-                            $fail  ('The ' . $attribute . ' is invalid.');
-                        }
-                    }
-                ],
-            ]);
+        $file_name = $validator->safe()->only(['file_name'])['file_name'];
+        Log::info("[SongController] Requesting to download file with name '" . $file_name . "'");
 
-            $file_name = $credentials['file_name'];
-            Log::info("[SongController] Requesting to download file with name '" . $file_name . "'");
-
-            $file_path = 'songs/' . $file_name;
-            if (!Storage::exists($file_path) || $file_name == ".gitkeep") {
-                Log::warning("[SongController] The file does not exist");
-                return response()->json(['error' => "File not found"], status: 404);
-            }
-
-            Log::info('[SongController] The file does exist');
-            return Storage::download($file_path);
-        } catch (ValidationException $e) {
-            Log::warning("[SongController] 'file_name' query parameter is not set.");
-            var_dump("Validator is not running");
-            return response()->json([
-                'error' => "Required 'file_name' query parameter is not set",
-                'message' => $e->getMessage()
-            ], 400);
+        $file_path = 'songs/' . $file_name;
+        if (!Storage::exists($file_path) || $file_name == ".gitkeep") {
+            Log::warning("[SongController] The file does not exist");
+            return response()->json(['error' => "File not found"], status: 404);
         }
+
+        Log::info('[SongController] The file does exist');
+        return Storage::download($file_path);
     }
 }
